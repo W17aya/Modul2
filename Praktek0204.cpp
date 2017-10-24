8ed32588e9efe280f88696ce830dd1124ab6bc83
Eko Wijaya <ekowijayawindows@gmail.com>
2017-10-25 01:56:38 +0800

    Membuat Inputan luas rumah

--- Praktek0204.cpp
+++ Praktek0204.cpp
@@ -5,4 +5,7 @@ using namespace std;
 int main(){
 	
 	double luas(42),jumlah_box(22.3885),luas_box(1.87596),biaya_parket(11194.3),biaya_pasang,total_biaya(12034.3);
+	
+	cout<<"Masukkan Luas Rumah(m2)\t:";
+	cin >> luas;
 }
